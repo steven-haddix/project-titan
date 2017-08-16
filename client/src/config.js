6c9c80330fc2abe7f8dbd96963a36c1ab589d137
Steven Haddix <steven.haddix@gmail.com>
2017-08-15 21:15:16 -0400

    Added correct GTM id to config

--- client/src/config.js
+++ client/src/config.js
@@ -9,7 +9,7 @@ const config = {
         apiUrl: 'https://iqm9qsbm9l.execute-api.us-east-1.amazonaws.com/dev/api',
         fbAppId: '991453140998882',
         googleClientId: '464712936089-q953apdu1bjiqtcjndktnnk1ts4f2cgv.apps.googleusercontent.com',
-        gtmId: 'GTM-WX5ZNVC',
+        gtmId: 'GTM-KFSX9C7',
         cognito: {
             USER_POOL_ID : 'us-east-1_2yevchWvf',
             APP_CLIENT_ID : '40037nss4qe8oornoi2ehlc2e6',
